1b44976776fc3ee6dfdd727cc6a30be64c98433b
Ramon Smits <ramonsmits@hotmail.com>
2019-07-18 22:19:54 +0100

    Sprite pixel art.

---
+++ swift projects/tile/Infrastructure/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Tile
+//
+//  Created by Ramon Smits on 22/01/2019.
+//  Copyright © 2019 Ramon Smits. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
